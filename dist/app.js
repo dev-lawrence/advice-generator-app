eb7067461e390850dd9ee7bcf1452aaeb747d9c4
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-05-25 21:47:14 +0100

    final commit

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,32 @@
 const button = document.querySelector('#btn');
 const adviceId = document.querySelector('.advice-id');
 const adviceOutput = document.querySelector('.advice-content');
+const body = document.querySelector('body');
 
-const showData = () => {
-  fetch('https://api.adviceslip.com/advice')
-    .then((res) => res.json())
-    .then((data) => {
-      const adviceData = data.slip;
-      adviceId.innerHTML = `<span class="advice-id">${adviceData.id}</span>`;
-      adviceOutput.innerHTML = `<h3 class="advice-text">“${adviceData.advice}”</h3>`;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const showAdvice = async () => {
+  try {
+    const response = await fetch('https://api.adviceslip.com/advice');
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch advice');
+    }
+
+    const data = await response.json();
+    const adviceSlip = data.slip;
+    const { id, advice } = adviceSlip;
+    adviceId.innerHTML = `<span class="advice-id">${id}</span>`;
+    adviceOutput.innerHTML = `<h3 class="advice-text">“${advice}”</h3>`;
+  } catch (error) {
+    const errorContainer = document.createElement('div');
+    errorContainer.classList.add('error-container');
+    errorContainer.innerHTML =
+      '<p class="error-message">Failed to fetch advice. Please try again later.</p>';
+    body.appendChild(errorContainer);
+
+    setTimeout(() => {
+      errorContainer.classList.remove('error-container');
+    }, 3000);
+  }
 };
 
-button.addEventListener('click', showData);
+button.addEventListener('click', showAdvice);
